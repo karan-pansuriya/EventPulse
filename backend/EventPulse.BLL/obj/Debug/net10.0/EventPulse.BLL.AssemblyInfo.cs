d6a3c1e8407264acecd70e35dc837018c3abc6b2
karan pansuriya <karanpansuriya0@gmail.com>
2026-05-12 15:56:23 +0530

    chore: update assembly informational version and add sourcelink files

--- backend/EventPulse.BLL/obj/Debug/net10.0/EventPulse.BLL.AssemblyInfo.cs
+++ backend/EventPulse.BLL/obj/Debug/net10.0/EventPulse.BLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventPulse.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0fa038910fe48e170b57f52d5958fa16d005a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventPulse.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventPulse.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
