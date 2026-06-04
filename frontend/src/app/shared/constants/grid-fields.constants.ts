298d314a222fff54efd985f64e3eb684947741e8
karan pansuriya <karanpansuriya0@gmail.com>
2026-06-04 10:18:20 +0530

    remove not used files

--- frontend/src/app/shared/constants/grid-fields.constants.ts
+++
@@ -1,8 +0,0 @@
-export const GRID_FIELDS = {
-  TICKET_ID: 'ticketId',
-  USER_NAME: 'userName',
-  EMAIL: 'email',
-  MOBILE: 'mobile',
-  DESCRIPTION: 'description',
-  SUBMISSION_DATE: 'submissionDate',
-};
